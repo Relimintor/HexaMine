a7b355d7d834d073457fe8b3b92f3e752eb7fcc1
Raptor4 <raptor4mc@gmail.com>
2026-04-03 21:52:23 +0200

    Update server.js

--- client/server.js
+++ client/server.js
@@ -1,28 +1,35 @@
+const express = require("express");
 const WebSocket = require("ws");
+const http = require("http");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
 const PORT = process.env.PORT || 3000;
-const server = new WebSocket.Server({ port: PORT });
+
+// Serve your game client
+app.use(express.static("public"));
 
 let players = [];
 
-server.on("connection", (ws) => {
-    console.log("Player connected");
+wss.on("connection", (ws) => {
+  console.log("Player connected");
+  players.push(ws);
 
-    players.push(ws);
-
-    ws.on("message", (msg) => {
-        // broadcast to all players
-        players.forEach(p => {
-            if (p !== ws && p.readyState === WebSocket.OPEN) {
-                p.send(msg.toString());
-            }
-        });
+  ws.on("message", (msg) => {
+    // Broadcast to all other players
+    players.forEach(p => {
+      if (p !== ws && p.readyState === WebSocket.OPEN) {
+        p.send(msg.toString());
+      }
     });
+  });
 
-    ws.on("close", () => {
-        players = players.filter(p => p !== ws);
-        console.log("Player disconnected");
-    });
+  ws.on("close", () => {
+    players = players.filter(p => p !== ws);
+    console.log("Player disconnected");
+  });
 });
 
-console.log("Server running on port", PORT);
+server.listen(PORT, () => console.log("Server running on port", PORT));
